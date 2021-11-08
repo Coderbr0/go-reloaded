9da42f7954f806324647cee0314b57a5db3a0ee7
Coderbro <mo.abbaszadi@gmail.com>
2021-11-08 20:22:32 +0000

    go-reloaded

--- main.go
+++ main.go
@@ -25,8 +25,7 @@ func ReadFile() []string {
 func main() {
 	fmt.Println(ReadFile()) // calling ReadFile() function and printing as output the text in sample.txt
 	strArr := ReadFile()
-	num := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println(n)
+	//num := []int{1, 2, 3, 4, 5, 6, 7, 8, 9} //for (low, number); (up, number); (cap, number)
 	fmt.Printf("\n")
 	var newWords []string
 	for i, word := range strArr { //this can also be written as i := range strArr {}; value can be omitted but not vice versa (index has to be omitted with underscore _ )
@@ -48,20 +47,22 @@ func main() {
 			newWords[len(newWords)-2] = strings.Title(strArr[i-2])
 			newWords[len(newWords)-1] = strings.Title(strArr[i-1])
 			continue
-		} else if word == "6)" {
-			continue
 		} else if word == "(low," && strArr[i+1] == "3)" {
 			newWords[len(newWords)-3] = strings.ToLower(strArr[i-3])
 			newWords[len(newWords)-2] = strings.ToLower(strArr[i-2])
 			newWords[len(newWords)-1] = strings.ToLower(strArr[i-1])
 			continue
-		} else if word == "3)" {
+		} else if len(word) > 1 && word[1] == ')' {
 			continue
 		} else if word == "," {
 			newWords[len(newWords)-1] += ","	//concatenates (adds) the comma to end of string; can also be written as newWords[len(newWords)-1] = newWords[len(newWords)-1] + ","
 			continue							//continue skips the extra "," that is read by the computer as a separate word due to the space between text and "," and moves on to the next word, therefore omitting the extra ","
-		} else if word == "(up," && strArr[i+1] == num {
-			for num :=  
+		} else if word == "(up," {
+			numWords := strArr[i+1][0]-48
+			wordsToModify := newWords[len(newWords)-int(numWords):] //alternative is strArr[i-int(numWords):i]    //when we have square brackets [] we are telling the compiler in Golang to look at what is before the square brackets and get the element at the stated index e.g. [i], [i-1] etc
+			newWords = newWords[:len(newWords)-int(numWords)] //to delete the words not required
+			newWords = append(newWords, strings.Split(strings.ToUpper(strings.Join(wordsToModify, " ")), " ")...) //strings.Join to turn slice into string; strings.Split to split string into elements (slice) to append later one by one which is done for consistency
+			continue 	//int(numWords):i we cast to int as without it we have bytes. i is at the end as we want uppercase up to the index and not after so that we don't uppercase other words
 		}
 		
 		// else if word == "." {
